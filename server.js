5b83ce2d74e8a1c61aa1ad0d18eaf48803433b4d
Matthew Weaber <matthewweaber89@gmail.com>
2019-10-06 14:19:52 -0400

    Init file setup, server setup, mongo setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res)=> {
+    res.json({
+        msg: 'Hello World, welcome to the Contact Keeper API'
+    });
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=> console.log(`App listening on http://localhost:${PORT}`));
