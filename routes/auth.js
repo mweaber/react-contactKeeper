6a118bb5550db0372973c583f2ae6e446cadb0a0
Matthew Weaber <matthewweaber89@gmail.com>
2019-10-07 19:32:14 -0400

    Worked in auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,72 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const User = require('../models/User');
+const { check, validationResult } = require('express-validator');
 
 // @route       GET api/auth
 // @desc        Get logged in user
 // @access      Private
-router.get('/', (req,res) => {
-    res.send('Get logged in user');
+router.get('/', (req, res) => {
+  res.send('Get logged in user');
 });
 
 // @route       Post api/auth
 // @desc        Auth user and get token
 // @access      Public
-router.post('/', (req,res) => {
-    res.send('Log in user');
-});
+router.post(
+  '/',
+  [check('email', 'Please include a valid email.').isEmail(), 
+   check('password', "Password is required").exists()],
+   async (req, res) => {
+    const errors = validationResult(req);
+    // If error then return bad request from user.
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    
+    const {email, password} = req.body;
+
+    try {
+        let user = await User.findOne({email})
+        
+        if(!user){
+            return res.status(400).json({msg:"Invalid Credentials"});
+        }
+
+        // Compare uses two params (plain-text pw, and the stored user password)
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch) {
+            return res.status(400).json({msg:'Invalid Credentials'});
+        }
+
+        const payload = {
+            user: {
+              id: user.id
+            }
+          };
+    
+          // Jwt token, set secret in config file. JWT takes in three params (payload, secret, callback)
+          jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            {
+              expiresIn: 360000
+            },
+            (err, token) => {
+              if (err) throw err;
+              // If no errors return token.
+              res.json({ token });
+            }
+          );
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,8 @@ const { check, validationResult } = require('express-validator');
 // @access      Public
 router.post(
   '/',
+  // Below is the express-validator
+  // Checking that json has all required info.
   [
     check('name', 'Please add name.')
       .not()
@@ -23,14 +25,17 @@ router.post(
       min: 6
     })
   ],
+  // Awaiting error messages from validation.
   async (req, res) => {
     const errors = validationResult(req);
+    // If error then return bad request from user.
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
     const { name, email, password } = req.body;
 
+    // Try/Catch for looking up user in DB
     try {
       let user = await User.findOne({ email });
 
@@ -44,29 +49,38 @@ router.post(
         password
       });
 
+      // Variable for bcrypt salt
       const salt = await bcrypt.genSalt(10);
 
+      // Hashing user password with bcrypt salt.
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
 
+      // Created payload for jwt in separate variable.
       const payload = {
-          user: {
-              id: user.id
-          }
-      }
+        user: {
+          id: user.id
+        }
+      };
 
-      jwt.sign(payload, config.get('jwtSecret'), {
-          expiresIn: 360000,   
-      }, (err, token) => {
-          if(err) throw err;
-          res.json({token});
-
-      })
-
+      // Jwt token, set secret in config file. JWT takes in three params (payload, secret, callback)
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          // If no errors return token.
+          res.json({ token });
+        }
+      );
     } catch (err) {
-        console.log(err.message);
-        res.status(500).send('Server Error');
+      console.log(err.message);
+      // You will get error if not in test in config for db. Atlas does not allow raw json from postman as admin in atlas.
+      res.status(500).send('Server Error');
     }
   }
 );
