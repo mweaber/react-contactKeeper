34fa57fb617698119c114266d32a040b1d0dfbef
Matthew Weaber <matthewweaber89@gmail.com>
2019-10-18 19:49:51 -0400

    Getting ready to deploy

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
+const path = require('path');
 
 // Connect Database
 connectDB();
 
 // Init Middleware
-app.use(express.json({extended: false}));
+app.use(express.json({ extended: false }));
 
-app.get('/', (req, res)=> {
-    res.json({
-        msg: 'Hello World, welcome to the Contact Keeper API'
-    });
+app.get('/', (req, res) => {
+  res.json({
+    msg: 'Hello World, welcome to the Contact Keeper API'
+  });
 });
 
 // Define Routes
@@ -19,6 +20,18 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=> console.log(`App listening on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`App listening on http://localhost:${PORT}`)
+);
