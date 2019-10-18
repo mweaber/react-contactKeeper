f1b709afd1161c88fb49c2d7fe9b7ac470306cb7
Matthew Weaber <matthewweaber89@gmail.com>
2019-10-18 19:56:56 -0400

    Removed test page

--- server.js
+++ server.js
@@ -9,12 +9,6 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.json({
-    msg: 'Hello World, welcome to the Contact Keeper API'
-  });
-});
-
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
